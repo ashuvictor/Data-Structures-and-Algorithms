3738481e8d32afe8e4daa78fcaedc0b6d65040ff
Ashu Singh <cashusingh2@gmail.com>
2022-03-16 11:46:12 +0530

    Practice

--- Dynamic Programming/ArrayBased/Medium/Sum of All Substrings of Number.cpp
+++ Dynamic Programming/ArrayBased/Medium/Sum of All Substrings of Number.cpp
@@ -1,3 +1,4 @@
+
 /*Sum of all substrings of a number 
 https://practice.geeksforgeeks.org/problems/sum-of-all-substrings-of-a-number-1587115621/1*/
 class Solution
@@ -18,3 +19,20 @@ class Solution
      return res;
     }
 };
+dp solution
+
+long long sumSubstrings(string s){
+       
+       // your code here
+       int n=s.size();
+       long long dp[n];
+       dp[0]=(s[0]-'0')%1000000007;
+       long long sum=dp[0];
+       for(int i=1;i<s.size();i++)
+       {
+        dp[i]=((i+1)*(s[i]-'0')+10*dp[i-1])%1000000007;   
+        sum=(sum+dp[i])%1000000007;
+       }
+       return sum%1000000007;
+   }
+

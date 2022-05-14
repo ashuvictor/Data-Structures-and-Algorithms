607265bcbffe4a9f454592472c8c41f1323158a4
Ashu Singh <cashusingh2@gmail.com>
2022-05-14 19:31:49 +0530

    Practice

--- Arrays/Easy/RotateArrayKbyRight.cpp
+++ Arrays/Easy/RotateArrayKbyRight.cpp
@@ -4,6 +4,22 @@ rotate the array to the right by k steps, where k is non-negative.
 https://leetcode.com/problems/rotate-array/*/
 class Solution {
 public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+      
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[(i+k)%n]=nums[i];
+        }
+        for(int i=0;i<n;i++)
+            nums[i]=ans[i];
+    }
+};
+
+
+
+class Solution {
+public:
    void reverse(vector<int>&nums,int low,int high){
        while(low<=high){
            swap(nums[low],nums[high]);

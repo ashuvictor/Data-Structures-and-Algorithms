79b0b9ffbf5c46b4787644a9a7a4f340716251ad
Ashu Singh <cashusingh2@gmail.com>
2022-08-06 14:51:42 +0530

    Practice

--- Searching And Sorting/Merge Sort.cpp
+++ Searching And Sorting/Merge Sort.cpp
@@ -1,87 +1,40 @@
-
-
-
-#include<iostream>
-using namespace std;
-
-void merge(int *arr, int s, int e) {
-
-    int mid = (s+e)/2;
-
-    int len1 = mid - s + 1;
-    int len2 = e - mid;
-
-    int *first = new int[len1];
-    int *second = new int[len2];
-
-    //copy values
-    int mainArrayIndex = s;
-    for(int i=0; i<len1; i++) {
-        first[i] = arr[mainArrayIndex++];
+class Solution
+{
+    public:
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+         int start=l,center=m,end=r;
+         int merg[r-l+1];int i=0;m++;
+         while(l<=center and m<=r)
+         {
+             if(arr[l]<=arr[m])
+             {
+                 merg[i++]=arr[l++];
+                 
+             }
+             else
+             {
+                 merg[i++]=arr[m++];
+             }
+         }
+         while(l<=center)
+         merg[i++]=arr[l++];
+         while(m<=r)
+         merg[i++]=arr[m++];
+         i=0;
+         for(int j=start;j<=end;j++)
+         arr[j]=merg[i++];
     }
-
-    mainArrayIndex = mid+1;
-    for(int i=0; i<len2; i++) {
-        second[i] = arr[mainArrayIndex++];
-    }
-
-    //merge 2 sorted arrays     
-    int index1 = 0;
-    int index2 = 0;
-    mainArrayIndex = s;
-
-    while(index1 < len1 && index2 < len2) {
-        if(first[index1] < second[index2]) {
-            arr[mainArrayIndex++] = first[index1++];
-        }
-        else{
-            arr[mainArrayIndex++] = second[index2++];
-        }
-    }   
-
-    while(index1 < len1) {
-        arr[mainArrayIndex++] = first[index1++];
-    }
-
-    while(index2 < len2 ) {
-        arr[mainArrayIndex++] = second[index2++];
-    }
-
-    delete []first;
-    delete []second;
-
-}
-
-void mergeSort(int *arr, int s, int e) {
-
-    //base case
-    if(s >= e) {
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l>=r)
         return;
+        int m=(l+r)>>1;
+     mergeSort(arr,l,m);
+     mergeSort(arr,m+1,r);
+     merge(arr,l,m,r);
     }
-    
-    int mid = (s+e)/2;
-
-    //left part sort karna h 
-    mergeSort(arr, s, mid);
-    
-    //right part sort karna h 
-    mergeSort(arr, mid+1, e);
-
-    //merge
-    merge(arr, s, e);
-
-}
-
-int main() {
-
-    int arr[15] = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
-    int n = 15;
-
-    mergeSort(arr, 0, n-1);
-
-    for(int i=0;i<n;i++){
-        cout << arr[i] << " ";
-    } cout << endl;
-
-    return 0;
-}
+};
--- Searching And Sorting/countInversions.cpp
+++ Searching And Sorting/countInversions.cpp
@@ -1,65 +1,49 @@
-/*
-Given an array of integers. Find the Inversion Count in the array. 
-
-Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
-Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
- 
-
-Example 1:
-
-Input: N = 5, arr[] = {2, 4, 1, 3, 5}
-Output: 3
-Explanation: The sequence 2, 4, 1, 3, 5 
-has three inversions (2, 1), (4, 1), (4, 3).
-https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1/
-*/
-brute force 
-o(n2)//iterate from 0 to n-1 and other from j=i+1 to n-1
-optimal
-using the merge sort algo
-order of elements doesnt matter what matters is the smaller element on right
 class Solution{
   public:
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    long long int count=0;
-    long long int inversionCount(long long arr[], long long N)
-    {
-        // Your Code Here
-        mergeSort(arr,0,N-1);
-        return count;
-    }
-    long long int* mergeSort(long long arr[],long long low,long long high){
-        if(low==high){
-            long long int* ans=new long long int[1];
-            ans[0]=arr[low];
-            return ans;
-        }
-        long long mid=low+(high-low)/2;
-        long long int* left=mergeSort(arr,low,mid);
-        long long int* right=mergeSort(arr,mid+1,high);
-        return mergeArrays(left,right,mid-low+1,high-mid);
-    }
-    long long int* mergeArrays(long long int* left,long long int* right,int n,int m){
-        int i=0,j=0;
-        long long int* ans=new long long int[n+m+1];
-        int k=0;
-        while(i<n and j<m){
-            if(left[i]<right[j])
-            ans[k++]=left[i++];
-            else
-            {ans[k++]=right[j++];
-            count+=n-i;//inversion count right side element is smaller
-                
+    void merge(long long arr[], long long int &count, long long int start, long long int mid, long long int end){
+        long long temp[end-start+1];
+        long long int i=start;
+        long long int j=mid+1;
+        long long int k=0;
+        while(i<=mid && j<=end){
+            if(arr[i]>arr[j]){
+                temp[k++]=arr[j++];
+                count+= mid-i+1;
+            }
+            else{
+                temp[k++]=arr[i++];
             }
         }
-            while(i<n)
-            ans[k++]=left[i++];
-            while(j<m)
-            ans[k++]=right[j++];
-            return ans;
         
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=end){
+            temp[k++]=arr[j++];
+        }
+        
+        for(long long int i=start; i<=end; i++){
+            arr[i]=temp[i-start];
+        }
+    }
+    void mergeSort(long long arr[], long long int &count, long long int start, long long int end){
+        if(start<end){
+            long long int mid=(start+end)/2;
+            mergeSort(arr,count,start,mid);
+            mergeSort(arr,count,mid+1,end);
+            merge(arr,count,start,mid,end);
+        }
+    }
+    long long int inversionCount(long long arr[], long long N)
+    {
+        // Your Code Her
+        long long int count=0;
+        mergeSort(arr,count,0,N-1);
+        return count;
+    
     }
 
 };

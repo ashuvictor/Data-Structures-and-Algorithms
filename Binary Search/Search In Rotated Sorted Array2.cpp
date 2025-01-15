963b175f4d23741f108fd99e53ab7a362aa3df5d
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2025-01-15 15:42:19 +0530

    Update Search In Rotated Sorted Array2.cpp

--- Binary Search/Search In Rotated Sorted Array2.cpp
+++ Binary Search/Search In Rotated Sorted Array2.cpp
@@ -20,30 +20,32 @@ Output: false
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        if(nums.size()==0)
-            return false;
-        int low=0;
-        int high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target)
-                return true;
-            if(nums[mid]>nums[low]){
-                if(target<nums[mid] and target>=nums[low])
-                    high=mid;
-                else
-                    low=mid+1;
-            }
-            else if(nums[mid]<nums[low]){
-                if(target>nums[mid] and target<nums[low])
-                    low=mid+1;
-                else
-                    high=mid;
-            }
-            else{
-                low++;  //third case that may be nums[mid]==nums[start]==nums[high]
-            }
+        int n = nums.size();
+int low = 0, high = n - 1;
+while (low <= high) {
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return true;
+    if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+        low++, high--;
+        continue;
+    }
+
+    if (nums[low] <= nums[mid]) {
+        if (nums[low] <= target && target <= nums[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
-        return false;
+    } else {
+        if (nums[mid] <= target && target <= nums[high]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+}
+return false;
+    }
+};
     }
 };

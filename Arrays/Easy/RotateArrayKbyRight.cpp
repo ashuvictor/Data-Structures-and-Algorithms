6613a9b29b21276ce058144d458a176ba92385a3
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2025-02-28 13:54:00 +0530

    Update RotateArrayKbyRight.cpp

--- Arrays/Easy/RotateArrayKbyRight.cpp
+++ Arrays/Easy/RotateArrayKbyRight.cpp
@@ -16,6 +16,21 @@ public:
     }
 };
 
+class Solution {
+public:
+    void rotateLeft(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; // In case k is greater than the size of the vector
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            ans[(i - k + n) % n] = nums[i];
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = ans[i];
+        }
+    }
+};
+
 
 
 class Solution {

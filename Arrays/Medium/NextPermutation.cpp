fb5f75cf1718643b9d04d9738ad3c5ca0beba536
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2026-02-26 13:24:56 +0530

    Update NextPermutation.cpp

--- Arrays/Medium/NextPermutation.cpp
+++ Arrays/Medium/NextPermutation.cpp
@@ -1,24 +1,44 @@
 /* NEXT PERMUTATION 
 https://leetcode.com/problems/next-permutation/
 */
+import java.util.*;
+
 class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        int sz=nums.size();
-        int s,k;
-        for( k=sz-2;k>=0;k--){
-            if(nums[k]<nums[k+1])
-                break;
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        int k;
+        // Step 1: Find first decreasing element from right
+        for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) break;
         }
-        if(k<0)
-            reverse(nums.begin(),nums.end());
-        else{
-            for(s=sz-1;s>k;s--){
-                if(nums[s]>nums[k])
-                    break;
+
+        // Step 2: If no pivot → reverse whole array
+        if (k < 0) {
+            reverse(nums, 0, n - 1);
+        } else {
+            // Step 3: Find next greater element from right
+            int s;
+            for (s = n - 1; s > k; s--) {
+                if (nums[s] > nums[k]) break;
             }
-            swap(nums[s],nums[k]);
-            reverse(nums.begin()+1+k,nums.end());
+
+            swap(nums, s, k);
+
+            // Step 4: Reverse suffix
+            reverse(nums, k + 1, n - 1);
         }
     }
-};
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
+}

283d3d50d45b9b261c9205802163877b9c503733
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2026-02-26 13:26:03 +0530

    Update SquaresSortedArray.cpp

--- Arrays/Medium/SquaresSortedArray.cpp
+++ Arrays/Medium/SquaresSortedArray.cpp
@@ -4,24 +4,26 @@ https://leetcode.com/problems/squares-of-a-sorted-array/
 */
 
 class Solution {
-public:
-    vector<int> sortedSquares(vector<int>& nums) {
-    int n=nums.size();
-        vector<int>ans(n);
-        int i=0,j=n-1;
-        int index=n-1;
-        while(i<=j){
-            int val1=nums[i]*nums[i];
-            int val2=nums[j]*nums[j];
-            if(val1>val2){
-                ans[index--]=val1;
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int i = 0, j = n - 1;
+        int index = n - 1;
+
+        while (i <= j) {
+            int val1 = nums[i] * nums[i];
+            int val2 = nums[j] * nums[j];
+
+            if (val1 > val2) {
+                ans[index--] = val1;
                 i++;
-            }
-            else{
-                ans[index--]=val2;
+            } else {
+                ans[index--] = val2;
                 j--;
             }
         }
+
         return ans;
     }
-};
+}

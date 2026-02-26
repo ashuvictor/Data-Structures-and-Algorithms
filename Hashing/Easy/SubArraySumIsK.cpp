5545c421c02fb6b5ec3cd7c0eb89f1d4de59796f
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2026-02-26 13:45:10 +0530

    Update SubArraySumIsK.cpp

--- Hashing/Easy/SubArraySumIsK.cpp
+++ Hashing/Easy/SubArraySumIsK.cpp
@@ -2,22 +2,26 @@
  return the total number of subarrays whose sum equals to k.
  https://leetcode.com/problems/subarray-sum-equals-k/
  */
- class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        int ans=0;
-        unordered_map<int,int>mp;
-        mp[0]=1;
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(mp.find(sum-k)!=mp.end())
-            ans+=mp[sum-k];
+ import java.util.*;
 
-                mp[sum]++;
- 
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); // base case
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-        return ans;
 
+        return ans;
     }
-};
+}

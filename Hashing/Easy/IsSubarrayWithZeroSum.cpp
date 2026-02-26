fa521928fd461a1e7345ffec2c095d8b73e2b91e
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2026-02-26 13:43:27 +0530

    Update IsSubarrayWithZeroSum.cpp

--- Hashing/Easy/IsSubarrayWithZeroSum.cpp
+++ Hashing/Easy/IsSubarrayWithZeroSum.cpp
@@ -1,21 +1,22 @@
 /*Subarray with zero sum
 https://techiedelight.com/practice/?problem=ZeroSum
 */
-class Solution
-{
-public:
-	bool hasZeroSumSubarray(vector<int> const &nums)
-	{
-		unordered_set<int>s;int sum=0;
-		
-		for(int i=0;i<nums.size();i++){
-			sum+=nums[i];
-			if(sum==0)
-			return true;
-			if(s.find(sum)!=s.end())
-			return true;
-			s.insert(sum);
-		}
-		return false;
-	}
-};
+import java.util.*;
+
+class Solution {
+    public boolean hasZeroSumSubarray(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (sum == 0) return true;
+            if (set.contains(sum)) return true;
+
+            set.add(sum);
+        }
+
+        return false;
+    }
+}

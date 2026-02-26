be186baeef460267ef21c4de00d2d641a0c723bb
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2026-02-26 13:30:28 +0530

    Update All A's before B's.cpp

--- Strings/Easy/All A's before B's.cpp
+++ Strings/Easy/All A's before B's.cpp
@@ -12,14 +12,13 @@ Output: true
 Explanation:
 The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
 Hence, every 'a' appears before every 'b' and we return true.
-
 class Solution {
-public:
-    bool checkString(string s) {
-        for(int i=1;i<s.size();i++){
-            if(s[i-1]=='b' and s[i]=='a')
+    public boolean checkString(String s) {
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == 'b' && s.charAt(i) == 'a') {
                 return false;
+            }
         }
         return true;
     }
-};
+}

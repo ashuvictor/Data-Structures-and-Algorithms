f3d7616fb40db7621ecf6802e152ec0e9ccf3f01
Ashu Singh <87942907+ashuvictor@users.noreply.github.com>
2026-02-26 13:25:39 +0530

    Update Sort012.cpp

--- Arrays/Medium/Sort012.cpp
+++ Arrays/Medium/Sort012.cpp
@@ -7,20 +7,31 @@ Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2]
 https://techiedelight.com/practice/?problem=DutchNationalFlagProblem
 */
 
-class Solution
-{
-public:
-	void sortArray(vector<int> &nums)
-	{
-		// Write your code here...
-		int low=0,mid=0;
-		int high=nums.size()-1;
-		while(mid<=high){
-			switch(nums[mid]){
-				case 0:swap(nums[low++],nums[mid++]);break;
-				case 1:mid++;break;
-				case 2:swap(nums[mid],nums[high--]);break;
-			}
-		}
-	}
-};
+class Solution {
+    public void sortArray(int[] nums) {
+        int low = 0, mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            switch (nums[mid]) {
+                case 0:
+                    swap(nums, low++, mid++);
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    swap(nums, mid, high--);
+                    break;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
